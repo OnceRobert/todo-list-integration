afa069d5c50f7d3226dacd9ad59a74222c2077eb
OnceRobert <robpurugganan@gmail.com>
2021-08-19 21:28:38 +0800

    [Todo List Integration][Angelo] test: added should_add_todo_item_when_add_given_todo_text

--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -43,6 +44,22 @@ class TodolistApplicationTests {
 				.andExpect(jsonPath("$[1].done").value(false))
 				.andExpect(jsonPath("$[2].text").doesNotExist());
 	}
+
+	@Test
+	void should_add_todo_item_when_add_given_todo_text() throws Exception {
+		String todo = "{"+
+				"        \"text\": \"Display Candybong\"\n" +
+				"    }";
+		//when
+		//then
+		mockMvc.perform(MockMvcRequestBuilders.post("/todos")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(todo))
+				.andExpect(jsonPath("$.text").value("Display Candybong"))
+				.andExpect(jsonPath("$.done").value(false));
+	}
+
+	
 
 
 

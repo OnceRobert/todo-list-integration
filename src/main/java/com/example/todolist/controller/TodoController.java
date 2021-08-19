d66b54acb4737a5bd806e6b688beb7489b7f117f
OnceRobert <robpurugganan@gmail.com>
2021-08-19 14:10:24 +0800

    [Todo List Integration][Angelo] feat: Added CrossOrigin annotation to TodoController to enable frontend Application using react-redux to access the data from the database

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.todolist.controller;
 
 import com.example.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class TodoController {
 
     public TodoController(TodoService todoService){this.todoService = todoService;}
 
+    @CrossOrigin
     @GetMapping()
     public List<Todo> getAllTodos(){
         return todoService.getTodoList();
